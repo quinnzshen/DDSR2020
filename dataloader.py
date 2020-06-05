9ec7f931662a85a3ce978d5f755af117a8d66b64
alien_lover <59703666+alienloverlegit@users.noreply.github.com>
2020-06-05 00:04:27 -0400

    Reformatted set_len(), and prepared for indexing. I'll do get_item tommorow.

--- dataloader.py
+++ dataloader.py
@@ -3,12 +3,15 @@ import torch
 from torch.utils.data import Dataset, DataLoader
 
 import os
+from glob import glob
 
 
 class KittiDataset(Dataset):
     def __init__(self, root_dir):
         self.root_dir = root_dir
-        self.num_frame_sum = []
+        self.len = -1
+        self.date_divisions = []
+        self.drive_divisions = []
         self.imagefiles = []
         self.velodyne = root_dir+'/velodyne_points'
         
@@ -16,33 +19,36 @@ class KittiDataset(Dataset):
         for filename in os.listdir(root_dir):
             if filename.startswith("image"):
                 self.imagefiles.append(filename)
-                
+
+
+
+    def set_len(self):
+        total = 0
+        # numFiles =0
+        dir_list = os.listdir(self.root_dir)
+        for direc in glob(self.root_dir + "/*/"):
+            self.date_divisions.append(total)
+            # iterating through all date folders
+            subtotal = 0
+            sub_drive_divisions = []
+            for sub_dir in glob(direc + "/*/"):
+                # iterating through all date_drive folders
+                with open(os.path.join(os.path.join(sub_dir, "velodyne_points"), "timestamps.txt")) as file:
+                    for _ in file:
+                        subtotal += 1
+
+                sub_drive_divisions.append(subtotal)
+
+            total += subtotal
+            self.drive_divisions.append(sub_drive_divisions)
+
+        self.len = total
 
     def __len__(self):
-        total = 0
-        numFiles =0
-        dir_list = os.listdir(self.root_dir)
-        for direc in dir_list:
-            # iterating through all date folders
-            level_1_path = os.path.join(self.root_dir, direc)
-            if os.path.isdir(level_1_path):
-                for sub_dir in os.listdir(level_1_path):
-                    # iterating through all date_drive folders
-                    level_2_path = os.path.join(level_1_path, sub_dir)
-                    #print(level_2_path)                  
-                    if os.path.isdir(level_2_path):
-                        for sub_sub_dir in os.listdir(level_2_path):
-                            # iterating through all date_drive.zip folders
-                            velo_folder = os.path.join(level_2_path, sub_sub_dir)
-                            if os.path.isfile(velo_folder):
-                                with open(os.path.join(level_1_path, "timestamps.txt")) as file:
-                                    for line in file:
-                                        total += 1
-                                    break
-                            break
-                    break
-            break
-        return total
+        if self.len < 0:
+            self.set_len()
+        return self.len
+        # return total
 
     def __getitem__(self, item):
         if torch.is_tensor(item):
@@ -63,5 +69,8 @@ class KittiDataset(Dataset):
                 pass
         return 1
 
+
 dataset = KittiDataset('data/kitti_example')
 print(len(dataset))
+
+# print(glob('data/kitti_example/2011_09_26/*/velodyne_points/'))
