5a9880860dde4722d0001cd7b3bcc1120767248b
aaronmarm <aamarm12@gmail.com>
2020-06-20 19:06:55 -0400

    Added documentation to all relevant functions
    - Also moved get_camera_intrinsic_dict into kitti_utils.py

--- compute_photometric_error_utils.py
+++ compute_photometric_error_utils.py
@@ -4,28 +4,10 @@ import os
 from matplotlib import pyplot as plt
 
 
-def get_camera_intrinsic_dict(calibration_dir): # Might be better off in kitti_utils.py
-    """
-    This function gets the intrinsic matrix for each camera from the KITTI claibation file
-    :param: [string] calibration_dir: directory where the KITTI calbration files are located
-    :return: dictionary of length 4 containing the 3x3 intrinsic matrix for each camera (keys: cam00, cam01, cam02, cam03)
-    """
-    # Load cam_to_cam calib file.
-    cam2cam = ku.read_calibration_file(os.path.join(calibration_dir, 'calib_cam_to_cam.txt'))
-
-    camera_intrinsic_dict = {}
-    KITTI_CAMERA_NAMES = ['00', '01', '02', '03']
-
-    for camera_name in KITTI_CAMERA_NAMES:
-        intrinsic_matrix = cam2cam[f"K_{camera_name}"].reshape(3,3)
-        camera_intrinsic_dict.update({f"cam{camera_name}" : intrinsic_matrix})
-    
-    return camera_intrinsic_dict
-
 def plot_sparse_image(lidar_point_coord_camera_image, image):
     """
     This function plots only the pixels of an image that have a corresponding lidar depth value.
-    :param: [numpy.array] lidar_point_coord_camera_image: [N, 3], contains lidar points on image plane, each row is format [X, Y, depth, 1]
+    :param: [numpy.array] lidar_point_coord_camera_image: [N, 4], contains lidar points on image plane, each row is format [X, Y, depth, 1]
     :param: [numpy.array] image: [H, W, 3], contains R, G, and B values for every pixel of the image that the user wants to plot.
     :return: None, plots a sparse image where only the pixels woth corresponding depth values are shown (pixels are dilated).
     """
@@ -43,39 +25,93 @@ def plot_sparse_image(lidar_point_coord_camera_image, image):
     plt.show()
 
 def get_depth_map_for_target_image(lidar_point_coord_camera_image, img_height, img_width):
-    # Create full-image depth map
+    """
+    This function creates a depth map the size of the image where each location contains the depth at that pixel, 
+    or zero if there is no ground-truth depth for that pixel.
+    :param: [numpy.array] lidar_point_coord_camera_image: [N, 4], contains lidar depth values for points on image plane, each row is format [X, Y, depth, 1].
+    :param: [int] im_height: Height of the image that a depth map is being produced for in pixels.
+    :param: [int] im_width: Width of the image that a depth map is being produced for in pixels.
+    :return: numpy.array of shape [im_height, im_width] that contains the lidar depth value for ech pixel location in the image, 
+    or zero if there is no depth value for a point.
+    """
+    # Create array of zeros that is the same shape as the image.
     full_image_depth_map = np.zeros((img_height, img_width))
+    # Add depth values at pixel points that have a corresponding lidar point.
     for row in lidar_point_coord_camera_image:
         full_image_depth_map[row[1]][row[0]] = row[2]
+    
     return full_image_depth_map
 
 def get_meshgrid(img_height, img_width):
-    # Create meshgrid.
+    """
+    This function creates a meshgrid, or an array where each location contains an array containing the homogeneous coordinates 
+    of that location, that is the same shape as the image.
+    :param: [int] im_height: Height of the image that a meshgrid is being produced for in pixels.
+    :param: [int] im_width: Width of the image that a meshgrid is being produced for in pixels.
+    :return: numpy.array of shape [im_height, im_width, 3] where each location in the array contains the coordinates of that 
+    location in the format [x, y, 1].
+    """
+    # Create array of zeros that is the same shape as the image with a 3x1 array at each of each locations.
     meshgrid = np.zeros((img_height, img_width, 3))
+    # Set the value at each location in the array to the homogeneous coordinates of that location
     for row in range(len(meshgrid)):
         for col in range (len(meshgrid[0])):
             meshgrid[row][col] = np.array([[row, col, 1]])
     return meshgrid
 
 def convert_source_pixel_coords_to_camera_frame(pixel_coordinates, full_image_depth_map, source_intrinsic, img_height, img_width):
+    """
+    This function projects the pixel coordinates of the source image into camera_frame.
+    :param: [numpy.array] pixel_coordinates: [im_height, im_width, 3], array that is the same shape as the source image, where 
+    each location contains an array of the homogenous pixel coordinates of that location, in format [x, y, 1]
+    :param: [numpy.array] full_image_depth_map: [im_height, im_width],  contains the lidar depth value for each pixel location in the image, 
+    or zero if there is no depth value for a point.
+    :param: [numpy_array] source_intrinsic: [3, 3], intrinsic matrix for the camera that captured the source image
+    :param: [int] im_height: Height of the source image in pixels.
+    :param: [int] im_width: Width of the source image  in pixels.
+    :return: numpy.array of shape [4, im_height, im_width] containing the camera coordinates for each pixel location in the 
+    source image (only includes camera coordinates for pixels that have corresponding ground truth depth values)
+    """
+    # Reshape depth and pixel coordinate arrays for multiplication.
     depth = np.reshape(full_image_depth_map, (1,-1))
     pixel_coordinates = pixel_coordinates.flatten().reshape((3, -1), order = 'F')
+    # Convert source pixel coordinates to camera coordinates.
     camera_coordinates = (np.linalg.inv(source_intrinsic) @ pixel_coordinates) * depth
-    
+    # Add a bottom row of ones to make camera coordinates homogeneous.
     ones = np.ones((1, img_height * img_width))
     camera_coordinates = np.vstack((camera_coordinates, ones))
+    # Reshape camera coordinates back to the shape of the source image.
     camera_coordinates = np.reshape(camera_coordinates, (-1, img_height, img_width))
+    
     return camera_coordinates
 
-def get_target_camera_frame_from_source_pixel_frame_matrix(rotation_source_to_target, translation_source_to_target, target_intrinsic):
+def get_target_pixel_frame_from_source_camera_frame_matrix(rotation_source_to_target, translation_source_to_target, target_intrinsic):
+    """
+    This function computes the matrix needed to convert source camera coordinates to target pixel coordinates.
+    :param: [numpy.array] rotation_source_to_target: [3, 3] matrix representing the relative rotation between the camera that 
+    captured the source image and the camera that captured the target image.
+    :param: [numpy.array] translation_source_to_target: [3, 1] vector representing the relative translation between the camera that 
+    captured the source image and the camera that captured the target image.
+    :param: [numpy.array] target_intrinsic: [3, 3] intrinsic matrix for the camera that captured the target image
+    :return: numpy.array of shape [4, 4] that contains matrix needed to convert source camera coordinates to target pixel coordinates.
+    """
+    # Overlay intrinsic matrix on 4x4 identity matrix.
     target_intrinsic_4x4 = np.eye(4)
     target_intrinsic_4x4[:3, :3] = target_intrinsic
+    # Compute relative pose matrix from relative rotation and relative translation.
     relative_pose_matrix = np.hstack((rotation_source_to_target, translation_source_to_target))
     relative_pose_matrix = np.vstack((relative_pose_matrix, np.array([[0, 0, 0, 1]])))
-    target_camera_frame_from_source_pixel_frame = target_intrinsic_4x4 @ relative_pose_matrix
-    return target_camera_frame_from_source_pixel_frame
+    # Multiply target intrinsic matrix by relative pose matrix.
+    target_camera_frame_from_source_camera_frame = target_intrinsic_4x4 @ relative_pose_matrix
+    return target_camera_frame_from_source_camera_frame
 
 def compute_relative_rotation_stereo(calibration_dir):
+    """
+    This function computes the relative rotation matrix between stereo cameras for KITTI.
+    :param: [string] calibration dir: directory where KITTI calibration files are located.
+    :return: numpy.array of shape [3, 3], matrix representing the relative rotation between the camera that captured the source 
+    image and the camera that captured the target image.
+    """
     # Read calibration file.
     cam2cam = ku.read_calibration_file(os.path.join(calibration_dir, 'calib_cam_to_cam.txt'))
 
@@ -83,9 +119,16 @@ def compute_relative_rotation_stereo(calibration_dir):
     rotation_target = cam2cam['R_rect_02'].reshape(3, 3)
     rotation_source = cam2cam['R_rect_03'].reshape(3,3)
     rotation_source_to_target = np.linalg.inv(rotation_source) @ rotation_target
+    
     return rotation_source_to_target
 
 def compute_relative_translation_stereo(calibration_dir):
+    """
+    This function computes the relative translation vector between stereo cameras for KITTI.
+    :param: [string] calibration dir: directory where KITTI calibration files are located.
+    :return: numpy.array of shape [3, 1], vector representing the relative translation between the camera that captured the source 
+    image and the camera that captured the target image.
+    """
      # Read calibration file.
     cam2cam = ku.read_calibration_file(os.path.join(calibration_dir, 'calib_cam_to_cam.txt'))
 
@@ -97,6 +140,17 @@ def compute_relative_translation_stereo(calibration_dir):
     return translation_source_to_target
 
 def project_source_camera_to_target_pixel_frame(camera_coordinates, target_camera_frame_from_source_pixel_frame, img_height, img_width):
+    """
+    This function projects the source camera coordinates into the target pixel frames.
+    :param: [numpy.array] camera_coordinates: [4, im_height, im_width] contains the camera coordinates for each pixel location in the 
+    source image (only includes camera coordinates for pixels that have corresponding ground truth depth values)
+    :param: [numpy.array] target_camera_frame_from_source_pixel_frame: [4, 4], contains matrix needed to convert source camera coordinates to target pixel coordinates.
+    :param: [int] im_height: Height of the source image in pixels.
+    :param: [int] im_width: Width of the source image  in pixels.
+    :return: numpy.array of hape [2, img_height, img_width] where each location is a source image pixel and the element in each 
+    location is a 2x1 array containing the target pixel coordinates that correspond to that source image pixel 
+    (only contains projected target pixel coordinates for source pixels that have a corresponding lidar point).
+    """
     camera_coordinates = np.reshape(camera_coordinates, (4, -1))
     target_pixel_coordinates = target_camera_frame_from_source_pixel_frame @ camera_coordinates
     target_pixel_coord_norm_x = target_pixel_coordinates[0, :] / target_pixel_coordinates[2, :]
