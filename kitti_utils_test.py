892d569737effd054cd0a972c4be241a4bb20ad3
Kevin Shen <kevinzshen3@gmail.com>
2020-07-01 18:54:15 -0700

    Made get_nearby_frames_data pytest sequential

--- kitti_utils_test.py
+++ kitti_utils_test.py
@@ -136,18 +136,16 @@ def test_get_nearby_frames(kitti_root_directory, kitti_dataset_index):
                        'stereo_left_capture_time_nsec', 
                        'stereo_right_image', 'stereo_right_shape', 
                        'stereo_right_capture_time_nsec']
+
     data = dataset[0]
-    data1 = dataset[1]
-    """
-    When index is 0 the frames -1 and -2 relative to the index do not exist and should return empty dictionaries
-    The keys returned for nearby_frames should be integers in the range(-previous_frames, next_frames + 1) excluding 0
-    The value of valid keys within nearby_frames should should be keys for each of the expected_fields
-    The value of invalid keys within nearby_frames should should return an empty list
-    """
+    # When idx = 0, [nearby_frames] keys: -1 and -2 should return empty dictionaries
     assert data['nearby_frames'][-1] == {}
     assert data['nearby_frames'][-2] == {}
+    # Keys for [nearby_frames] should be int values within range(-previous_frames, next_frames + 1) with exception of 0
     assert list(data['nearby_frames'].keys()) == [-2, -1, 1, 2]
+    # Values of valid [nearby_frames] keys should be elements of [expected_fields]
     assert list(data['nearby_frames'][1].keys()) == expected_fields
+    # Values of invalid [nearby_frames] keys should be empty dictionaries
     assert list(data['nearby_frames'][-1].keys()) == []
     
     """
@@ -156,10 +154,14 @@ def test_get_nearby_frames(kitti_root_directory, kitti_dataset_index):
     The keys returned for nearby_frames should be integers in the range(-previous_frames, next_frames + 1) excluding 0
     The value of valid keys within nearby_frames should should be keys for each of the expected_fields
     """
-    assert data1['nearby_frames'][-1] != {}
-    assert data1['nearby_frames'][-2] == {}
-    assert list(data1['nearby_frames'].keys()) == [-2, -1, 1, 2]
-    assert list(data1['nearby_frames'][-1].keys()) == expected_fields
+    data = dataset[1]
+    # When idx = 0, [nearby_frames] keys: -1 should return camera data, while -2 should return an empty dictionary
+    assert data['nearby_frames'][-1] != {}
+    assert data['nearby_frames'][-2] == {}
+    # Keys for [nearby_frames] should be int values within range(-previous_frames, next_frames + 1)  with exception of 0
+    assert list(data['nearby_frames'].keys()) == [-2, -1, 1, 2]
+    # Values of valid [nearby_frames] keys should be elements of [expected_fields]
+    assert list(data['nearby_frames'][-1].keys()) == expected_fields
     
 def test_get_camera_intrinsic_dict():
     sample_cam_intrinsic_dict = ku.get_camera_intrinsic_dict(EXAMPLE_CALIBRATION_DIR)
