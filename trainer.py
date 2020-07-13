3f90ab6b90e1e9e5d18c77478045df9ea37a01b8
alex417118 <43454023+alex417118@users.noreply.github.com>
2020-07-12 23:03:37 -0400

    Created trainer file, added imports

---
+++ trainer.py
@@ -0,0 +1,9 @@
+from dataloader import KittiDataset
+from loss import calc_loss
+import sys
+sys.path.append('third_party\monodepth2')
+import ResnetEncoder, DepthDecoder, layers
+
+class Trainer
+    def    
+dataset = KittiDataset.init_from_config('configs\kitti_dataset')
