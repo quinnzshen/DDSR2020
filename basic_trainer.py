6b72dbd034f608f019a4ee9079d235341c6aba70
alien_lover <59703666+alienloverlegit@users.noreply.github.com>
2020-07-31 12:12:52 -0400

    Fixed some potential issues

--- basic_trainer.py
+++ basic_trainer.py
@@ -32,9 +32,9 @@ class Trainer:
             self.config = yaml.load(file, Loader=yaml.Loader)
 
         # GPU/CPU setup
-        #self.device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+        # self.device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
         self.device = "cpu"
-        
+
         # Epoch and batch info
         self.num_epochs = self.config["num_epochs"]
         self.batch_size = self.config["batch_size"]
@@ -48,12 +48,14 @@ class Trainer:
 
         train_config_path = self.config["train_config_path"]
         self.train_dataset = KittiDataset.init_from_config(train_config_path)
-        self.train_dataloader = DataLoader(self.train_dataset, batch_size=self.batch_size, shuffle=False, collate_fn=self.collate)
+        self.train_dataloader = DataLoader(self.train_dataset, batch_size=self.batch_size, shuffle=False,
+                                           collate_fn=self.collate)
 
         valid_config_path = self.config["valid_config_path"]
         self.valid_dataset = KittiDataset.init_from_config(valid_config_path)
-        self.valid_dataloader = DataLoader(self.valid_dataset, batch_size=len(self.valid_dataset), shuffle=False, collate_fn=self.collate)
-        
+        self.valid_dataloader = DataLoader(self.valid_dataset, batch_size=self.batch_size, shuffle=False,
+                                           collate_fn=self.collate)
+
         # Neighboring frames
         self.prev_frames = self.train_dataset.previous_frames
         self.next_frames = self.train_dataset.next_frames
@@ -97,56 +99,58 @@ class Trainer:
 
         self.save_model()
         print('Model saved.')
-    
+
     def run_epoch(self):
         """
         Runs a single epoch of training and validation
         """
-        
+
         # Training
         train_start_time = time.time()
 
-        print("Training epoch {}".format(self.epoch + 1), end=", ")
+        print(f"Training epoch {self.epoch + 1}", end=", ")
 
         self.models['resnet_encoder'].train()
         self.models['depth_decoder'].train()
-        
+
         img_num = 1
-                
+
+        total_loss = 0
         for batch_idx, batch in enumerate(self.train_dataloader):
-            losses = self.process_batch(batch_idx, batch, img_num, len(self.train_dataset), True)
-        
-        self.writer.add_scalar("Training" +  ' Loss', losses.item(), self.epoch) 
-        
+            total_loss += self.process_batch(batch_idx, batch, img_num, len(self.train_dataset), True).item()
+        total_loss /= batch_idx + 1
+
+        self.writer.add_scalar("Training" + ' Loss', total_loss, self.epoch)
+
         self.lr_scheduler.step()
-        
+
         train_end_time = time.time()
-        
-        print("Training Loss: {}".format(losses.item()))
-        print("Time spent: {}".format(train_end_time - train_start_time))
-        
-        
+
+        print(f"Training Loss: {total_loss}")
+        print(f"Time spent: {train_end_time - train_start_time}\n")
+
         # Validation
         valid_start_time = time.time()
 
-        print("Validating epoch {}".format(self.epoch + 1), end=", ")
+        print(f"Validating epoch {self.epoch + 1}", end=", ")
 
         self.models['resnet_encoder'].eval()
         self.models['depth_decoder'].eval()
 
         img_num = 1
-         
+
+        total_loss = 0
         for batch_idx, item in enumerate(self.valid_dataloader):
             with torch.no_grad():
-                losses = self.process_batch(batch_idx, item, img_num, len(self.valid_dataset), False)
-        
-        self.writer.add_scalar("Validation" +  ' Loss', losses.item(), self.epoch) 
+                total_loss += self.process_batch(batch_idx, item, img_num, len(self.valid_dataset), False).item()
+        total_loss /= batch_idx
+
+        self.writer.add_scalar("Validation" + ' Loss', total_loss, self.epoch)
 
         valid_end_time = time.time()
-        
-        print("Validation Loss: {}".format(losses.item()))
-        print("Time spent: {}".format(valid_end_time - valid_start_time))
-        print()
+
+        print(f"Validation Loss: {total_loss}")
+        print(f"Time spent: {valid_end_time - valid_start_time}\n\n")
 
     def process_batch(self, batch_idx, batch, img_num, dataset_length, train):
         """
@@ -158,9 +162,10 @@ class Trainer:
         :param [boolean] train: Differentiates between training and validation
         :return [tensor] losses: A 0-dimensional tensor containing the loss of the batch
         """
-    
+
         # Predict disparity map
-        inputs = F.interpolate(batch["stereo_left_image"].to(self.device).permute(0, 3, 1, 2).float(), (self.height, self.width), mode="bilinear", align_corners=False)
+        inputs = F.interpolate(batch["stereo_left_image"].to(self.device).permute(0, 3, 1, 2).float(),
+                               (self.height, self.width), mode="bilinear", align_corners=False)
         features = self.models['resnet_encoder'](inputs)
         outputs = self.models['depth_decoder'](features)
         disp = outputs[("disp", 0)]
@@ -173,17 +178,21 @@ class Trainer:
         # Convert disparity to depth
         _, depths = disp_to_depth(disp, 0.1, 100)
         outputs[("depths", 0)] = depths
-        
+
         # Source image and pose data
         tgt_poses = batch["pose"].to(self.device)
-        sources_list = [F.interpolate(batch["stereo_right_image"].to(self.device).permute(0, 3, 1, 2).float(), (self.height, self.width), mode="bilinear", align_corners=False)]
+        sources_list = [F.interpolate(batch["stereo_right_image"].to(self.device).permute(0, 3, 1, 2).float(),
+                                      (self.height, self.width), mode="bilinear", align_corners=False)]
         poses_list = [batch["rel_pose_stereo"].to(self.device)]
 
         for i in range(-self.prev_frames, self.next_frames + 1):
             if i == 0:
                 continue
 
-            sources_list.append(F.interpolate(batch["nearby_frames"][i]["camera_data"]["stereo_left_image"].to(self.device).permute(0, 3, 1, 2).float(), (self.height, self.width), mode="bilinear", align_corners=False))
+            sources_list.append(F.interpolate(
+                batch["nearby_frames"][i]["camera_data"]["stereo_left_image"].to(self.device).permute(0, 3, 1,
+                                                                                                      2).float(),
+                (self.height, self.width), mode="bilinear", align_corners=False))
             poses_list.append(
                 torch.matmul(torch.inverse(tgt_poses), batch["nearby_frames"][i]["pose"].to(self.device)))
 
@@ -216,24 +225,24 @@ class Trainer:
                         "disparities": disp,
                         "initial_masks": mask
                         }
-        losses = calc_loss(loss_inputs, loss_outputs)    
-        
+        losses = calc_loss(loss_inputs, loss_outputs)
+
         # Backpropogates if train is set to True
-        if train == True:
+        if train:
             self.optimizer.zero_grad()
             losses.backward()
             self.optimizer.step()
-            
+
         # Adjusts image number    
         img_num += self.batch_size
-        
+
         return losses
-    
+
     def save_model(self):
         """
         Saves model weights to disk (from monodepth2 repo)
         """
-        
+
         save_folder = os.path.join("models", "weights_{}".format(self.epoch))
         if not os.path.exists(save_folder):
             os.makedirs(save_folder)
@@ -264,16 +273,17 @@ class Trainer:
         mapper = cm.ScalarMappable(norm=normalizer, cmap='magma')
         colormapped_im = (mapper.to_rgba(disp_resized_np)[:, :, :3] * 255).astype(np.uint8)
         im = transforms.ToTensor()(colormapped_im)
-        
+
         # Decides whether to add disparity map to the training or validation set
-        if train == True:
+        train_or_val = "Validation"
+        if train:
             train_or_val = "Training"
-        else:
-            train_or_val = "Validation"
-            
+
         # Add image to tensorboard
-        self.writer.add_image(train_or_val + " - " + 'Epoch: {}, '.format(self.epoch + 1) + 'Image: {}'.format(img_num), im,
+        self.writer.add_image(train_or_val + " - " + f'Epoch: {self.epoch + 1}, ' + f'Image: {img_num}',
+                              im,
                               self.epoch * dataset_length + img_num)
+
 
 def disp_to_depth(disp, min_depth, max_depth):
     """
@@ -284,7 +294,7 @@ def disp_to_depth(disp, min_depth, max_depth):
     :param [int] min_depth: The minimum depth value
     :param [int] max_depth: The maximum depth value
     """
-    
+
     min_disp = 1 / max_depth
     max_disp = 1 / min_depth
     scaled_disp = min_disp + (max_disp - min_disp) * disp
