ff2f087e4af7e4246fea03fa2af464d193aa7b15
alex417118 <43454023+alex417118@users.noreply.github.com>
2020-08-03 17:49:35 -0400

    Update get_lengths.py

--- get_lengths.py
+++ get_lengths.py
@@ -1,24 +1,25 @@
 import os
-root_dir = 'data/kitti_data'
+root_dir = 'data/kitti_example'
 directories = []
 lengths = []
 for folder in os.listdir(root_dir):
     if folder.endswith("zip") == False:
         new_dir = os.path.join(root_dir, folder)
         for folder2 in os.listdir(new_dir):
-            new_dir2 = os.path.join(new_dir, folder2)
-            directories.append(os.path.join(folder, folder2))
-            for folder3 in os.listdir(new_dir2):
-                new_dir3 = os.path.join(new_dir2, folder3)
-                for folder4 in os.listdir(new_dir3):
-                    new_dir4 = os.path.join(new_dir3, folder4)
-                    count = 0
-                    for folder5 in os.listdir(new_dir4):
-                        count+=1
-                    lengths.append(count)
+            if folder2.endswith("txt") == False:
+                new_dir2 = os.path.join(new_dir, folder2)
+                directories.append(os.path.join(folder, folder2))
+                for folder3 in os.listdir(new_dir2):
+                    new_dir3 = os.path.join(new_dir2, folder3)
+                    for folder4 in os.listdir(new_dir3):
+                        new_dir4 = os.path.join(new_dir3, folder4)
+                        count = 0
+                        for folder5 in os.listdir(new_dir4):
+                            count+=1
+                        lengths.append(count)
+                        break
                     break
                 break
             break
-        break
 for i, direc in enumerate(directories):
     print(direc + " " + str(lengths[i]))
