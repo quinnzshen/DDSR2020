c2716aa1aa7a423d1f0e11d4f876784743e84df2
alex417118 <43454023+alex417118@users.noreply.github.com>
2020-08-03 11:20:57 -0400

    Got rid of print

--- kitti_utils.py
+++ kitti_utils.py
@@ -157,7 +157,6 @@ def get_camera_data(path_name, idx):
         # The f-string is following the format of KITTI, padding the frame number with 10 zeros.
         camera_image_path = os.path.join(path_name, f"{camera_path}/data/{idx:010}.png")
         timestamp_path = os.path.join(path_name, f"{camera_path}/timestamps.txt")
-        print(camera_image_path)
         if os.path.exists(camera_image_path) and os.path.exists(timestamp_path):
             camera_image = torch.from_numpy(np.asarray(Image.open(camera_image_path)))
             timestamp = get_timestamp_nsec(timestamp_path, idx)
