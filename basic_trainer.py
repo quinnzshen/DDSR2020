318fff78fe677e28d11706548ffb400214616656
alien_lover <59703666+alienloverlegit@users.noreply.github.com>
2020-08-04 13:37:33 -0400

    Added dynamic scaling for intrinsic matrices

--- basic_trainer.py
+++ basic_trainer.py
@@ -87,9 +87,6 @@ class Trainer:
         weight_decay = self.config["weight_decay"]
         self.lr_scheduler = optim.lr_scheduler.StepLR(self.optimizer, scheduler_step_size, weight_decay)
 
-        # Reading from JPEG
-        self.is_jpeg = self.config["read_jpeg"]
-
         # Writer for tensorboard
         self.writer = SummaryWriter()
 
@@ -230,10 +227,13 @@ class Trainer:
         src_intrinsics_stereo = batch["intrinsics"]["stereo_right"].to(self.device)
 
         # Adjust intrinsics based on input size
-        tgt_intrinsics[:, 0] = tgt_intrinsics[:, 0] * (self.width / 1242)
-        tgt_intrinsics[:, 1] = tgt_intrinsics[:, 1] * (self.height / 375)
-        src_intrinsics_stereo[:, 0] = src_intrinsics_stereo[:, 0] * (self.width / 1242)
-        src_intrinsics_stereo[:, 1] = src_intrinsics_stereo[:, 1] * (self.height / 375)
+        shapes = batch["shapes"].to(self.device).float()
+        out_shape = torch.tensor([self.height, self.width]).to(self.device)
+        shapes = out_shape / shapes
+        tgt_intrinsics[:, 0] = tgt_intrinsics[:, 0] * shapes[:, 1].reshape(-1, 1)
+        tgt_intrinsics[:, 1] = tgt_intrinsics[:, 1] * shapes[:, 0].reshape(-1, 1)
+        src_intrinsics_stereo[:, 0] = src_intrinsics_stereo[:, 0] * shapes[:, 1].reshape(-1, 1)
+        src_intrinsics_stereo[:, 1] = src_intrinsics_stereo[:, 1] * shapes[:, 0].reshape(-1, 1)
 
         intrinsics = [src_intrinsics_stereo]
         for i in range(len(poses_list) - 1):
@@ -331,5 +331,5 @@ def disp_to_depth(disp, min_depth, max_depth):
 
 
 if __name__ == "__main__":
-    test = Trainer("configs/full_model.yml")
+    test = Trainer("configs/basic_model.yml")
     test.train()
