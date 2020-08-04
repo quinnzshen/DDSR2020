4025cdc74e878e010a224fabec3b592105fc4854
alien_lover <59703666+alienloverlegit@users.noreply.github.com>
2020-08-03 20:47:54 -0400

    blebebl

--- tempbruh.py
+++ tempbruh.py
@@ -5,8 +5,11 @@ if __name__ == "__main__":
     train_config_path = "configs/full_train_dataset.yml"
     data = KittiDataset.init_from_config(train_config_path)
 
-    print(get_pose("2011_10_03/2011_10_03_drive_0034_sync", 1757))
+    print("start")
+    print(get_pose("data/kitti_data/2011_10_03/2011_10_03_drive_0034_sync", 1757))
 
+
+    print("end")
 
 
     # print(data[0].keys())
