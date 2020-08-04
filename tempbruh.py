bb3c3fb21756195c9b14af0fe237d61849328097
alien_lover <59703666+alienloverlegit@users.noreply.github.com>
2020-08-03 20:46:59 -0400

    blebebl

--- tempbruh.py
+++ tempbruh.py
@@ -1,7 +1,12 @@
 from kitti_dataset import KittiDataset
+from kitti_utils import get_pose
 
 if __name__ == "__main__":
     train_config_path = "configs/full_train_dataset.yml"
     data = KittiDataset.init_from_config(train_config_path)
 
-    print(data[0].keys())
+    print(get_pose("2011_10_03/2011_10_03_drive_0034_sync", 1757))
+
+
+
+    # print(data[0].keys())
