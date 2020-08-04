9c024bb8e4fe1aac2fd808fa4890f6ee1bb5fecd
alien_lover <59703666+alienloverlegit@users.noreply.github.com>
2020-08-03 20:16:45 -0400

    hablo

--- kitti_utils.py
+++ kitti_utils.py
@@ -163,6 +163,8 @@ def get_camera_data(path_name, idx):
             camera_data[f"{camera_name}_image"] = camera_image
             camera_data[f"{camera_name}_shape"] = camera_image.shape
             camera_data[f"{camera_name}_capture_time_nsec"] = timestamp
+    else:
+        print("YO HABLO ESPANOL")
     return camera_data
 
 
