b4ba223adc8fef946cde2847ba0282355ca5b712
aaronmarm <aamarm12@gmail.com>
2020-08-19 19:06:07 -0400

    update docstrings

--- depth_hints/networks/fpn.py
+++ depth_hints/networks/fpn.py
@@ -3,6 +3,12 @@ import torch.nn as nn
 
 class FPN(nn.Module):
     def __init__(self, num_ch_pyramid, device):
+        """
+        Initializes a feature pyramid network based on the implementation described in the paper 
+        Feature Pyramid Networks for Object Detection (https://arxiv.org/pdf/1612.03144.pdf)
+        :param [np.array] num_ch_pyramid: [N], contains the number of channels of each layer for the output of the pyramid.
+        :param [torch.device] device: device (either cuda or cpu) being used. 
+        """
         super(FPN, self).__init__()
         self.device = device
         self.upsampler = nn.Upsample(scale_factor=2, mode='bilinear', align_corners=False)
@@ -11,15 +17,21 @@ class FPN(nn.Module):
         
     
     def forward(self, input_features):
+        """
+        This function performs a forward pass in the FPN network.
+        :param input_features [list]: List of an arbitrary number of feature maps.
+        :return pyramid [list]: List of feature maps that is the same length as input_features where the number of channels of 
+        each feature map matches self.num_ch_pyramid.
+        """
         top_down = input_features[::-1]
         pyramid = [top_down[0]]
         input_features.reverse()
         
         for idx in range(1, len(input_features)):
             conv = nn.Conv2d(input_features[idx].shape[1], top_down[idx-1].shape[1], 1)
-            conv.to(self.device) # change to be adaptable
-            tmp = conv(input_features[idx])
-            level = self.upsampler(top_down[idx-1]) + tmp
+            conv.to(self.device)
+            bottum_up_convoluted = conv(input_features[idx])
+            level = self.upsampler(top_down[idx-1]) + bottom_up_convoluted
             pyramid.append(level)
         pyramid.reverse()
         return pyramid
