3f6563bc5874c301f3e2e8334e0ee88b1e71a8cf
Ubuntu <ubuntu@ip-172-31-73-168.ec2.internal>
2020-08-19 19:22:28 +0000

    debugging

--- depth_hints/networks/__init__.py
+++ depth_hints/networks/__init__.py
@@ -3,4 +3,4 @@ from .depth_decoder import DepthDecoder
 from .pose_decoder import PoseDecoder
 from .pose_cnn import PoseCNN
 from .fpn import FPN
-from .dense_network] import DenseNetwork
+from .dense_network import DenseNetwork
