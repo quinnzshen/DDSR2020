c243ead9a47641bfb2438b10a88234644bc8ea93
alien_lover <59703666+alienloverlegit@users.noreply.github.com>
2020-08-28 11:51:34 -0400

    Edited FPN to be updating weights, and cleaned up code

--- depth_hints/networks/fpn.py
+++ depth_hints/networks/fpn.py
@@ -1,37 +1,35 @@
 import torch
 import torch.nn as nn
+import numpy as np
+
 
 class FPN(nn.Module):
-    def __init__(self, num_ch_pyramid, device):
+    def __init__(self, num_ch_enc):
         """
         Initializes a feature pyramid network based on the implementation described in the paper 
         Feature Pyramid Networks for Object Detection (https://arxiv.org/pdf/1612.03144.pdf)
-        :param [np.array] num_ch_pyramid: [N], contains the number of channels of each layer for the output of the pyramid.
-        :param [torch.device] device: device (either cuda or cpu) being used. 
+        :param [np.array] num_ch_enc: [N], contains the number of channels of each layer for the output of the pyramid.
         """
         super(FPN, self).__init__()
-        self.device = device
         self.upsampler = nn.Upsample(scale_factor=2, mode='bilinear', align_corners=False)
         self.upsampler.to(self.device)
-        self.num_ch_pyramid = num_ch_pyramid
-        
-    
+        self.num_ch_pyramid = np.roll(num_ch_enc, -1)
+        self.num_ch_pyramid[-1] = self.num_ch_pyramid[-2]
+        self.convs = []
+        for i in range(len(num_ch_enc) - 1):
+            self.convs.append(nn.Conv2d(num_ch_enc[i], num_ch_enc[i+1], 1))
+        self.fpn = nn.ModuleList(self.convs)
+
     def forward(self, input_features):
         """
         This function performs a forward pass in the FPN network.
-        :param input_features [list]: List of an arbitrary number of feature maps.
-        :return pyramid [list]: List of feature maps that is the same length as input_features where the number of channels of 
+        :param [list] input_features: List of an arbitrary number of feature maps.
+        :return [list]: List of feature maps that is the same length as input_features where the number of channels of
         each feature map matches self.num_ch_pyramid.
         """
-        top_down = input_features[::-1]
-        pyramid = [top_down[0]]
-        input_features.reverse()
-        
-        for idx in range(1, len(input_features)):
-            conv = nn.Conv2d(input_features[idx].shape[1], top_down[idx-1].shape[1], 1)
-            conv.to(self.device)
-            bottum_up_convoluted = conv(input_features[idx])
-            level = self.upsampler(top_down[idx-1]) + bottom_up_convoluted
-            pyramid.append(level)
-        pyramid.reverse()
+        pyramid = []
+        for i in range(len(input_features) - 1):
+            pyramid.append(self.upsampler(input_features[i+1]) + self.convs[i](input_features[i]))
+        pyramid.append(input_features[-1])
+
         return pyramid
