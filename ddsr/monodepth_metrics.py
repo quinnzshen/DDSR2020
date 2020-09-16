896b1bc4ef12431eda24fd3ac869515e80569915
alien_lover <59703666+alienloverlegit@users.noreply.github.com>
2020-09-16 09:01:27 -0400

    Undid deletion of os.join

--- ddsr/monodepth_metrics.py
+++ ddsr/monodepth_metrics.py
@@ -60,7 +60,7 @@ def run_metrics(log_dir, epoch):
     MAX_DEPTH = 80
 
     # Load data from config
-    config_path = os.path.join(log_dir)
+    config_path = os.path.join(log_dir, "config.yml")
     with open(config_path) as file:
         config = yaml.load(file, Loader=yaml.Loader)
 
