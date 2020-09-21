bebe2745cff9a17d39538517b4da5bfbd2e68069
alex417118 <43454023+alex417118@users.noreply.github.com>
2020-09-20 21:53:30 -0400

    Update temp_metrics.py

--- ddsr/temp_metrics.py
+++ ddsr/temp_metrics.py
@@ -91,7 +91,7 @@ def run_metrics(log_dir, epoch):
 
     with torch.no_grad():
         for batch in dataloader:
-            inputs = batch["stereo_left_image"].to(device).float()
+            inputs = batch["stereo_left_image"].to(device).float() * 255.0
 
             if config.get("use_fpn"):
                 output = models["depth_decoder"](models["fpn"](models["depth_encoder"](inputs)))
