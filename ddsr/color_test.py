7ea74c47a3563e2298ee462e0c91eb95cca417c9
alien_lover <59703666+alienloverlegit@users.noreply.github.com>
2020-09-22 09:31:49 -0400

    More testing for eventual implementation

--- ddsr/color_test.py
+++ ddsr/color_test.py
@@ -6,19 +6,20 @@ from kitti_utils import get_camera_data
 
 
 
-
-
+pilimage = np.array(Image.open("data/kitti_example/2011_09_26/2011_09_26_drive_0048_sync/image_02/data/0000000000.png"))
+print(pilimage)
 
 d = get_camera_data("data/kitti_example/2011_09_26/2011_09_26_drive_0048_sync", 11, is_jpeg=False)
-image = np.array(d["stereo_left_image"])
+image = np.array(d["stereo_left_image"]) * 255
 print(image.shape)
-
+print(image)
 himage = cv2.cvtColor(image, cv2.COLOR_RGB2HSV)
 
 print(himage.shape)
 
 
-print(d["stereo_left_image"])
+print(himage)
+print(himage.max(0).max(0))
 
 
 
