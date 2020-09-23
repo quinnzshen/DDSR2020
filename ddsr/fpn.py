30a4a8e09d660870c296e47c26c432c38758592a
alien_lover <59703666+alienloverlegit@users.noreply.github.com>
2020-09-23 09:11:19 -0400

    Changed init and forward of pyramid

--- ddsr/fpn.py
+++ ddsr/fpn.py
@@ -14,10 +14,10 @@ class FPN(nn.Module):
         self.upsampler = nn.Upsample(scale_factor=2, mode='nearest', align_corners=False)
         self.num_ch_pyramid = [num_ch_out] * len(num_ch_enc)
         # self.convs = []
-        self.layers = nn.ModuleDict({str(len(num_ch_enc) - 1) + "_1x1conv": nn.Conv2d(num_ch_enc[len(num_ch_enc) - 1], num_ch_out, 1)})
-        for i in range(len(num_ch_enc) - 2, -1, -1):
-            self.layers[str(i - 1) + "_3x3conv"] = nn.Conv2d(num_ch_enc[i], num_ch_out, 3)
+        # self.layers = nn.ModuleDict({str(len(num_ch_enc) - 1) + "_1x1conv": nn.Conv2d(num_ch_enc[len(num_ch_enc) - 1], num_ch_out, 1)})
+        for i in range(len(num_ch_enc) - 1, -1, -1):
             self.layers[str(i) + "_1x1conv"] = nn.Conv2d(num_ch_enc[i], num_ch_out, 1)
+            self.layers[str(i) + "_3x3conv"] = nn.Conv2d(num_ch_out, num_ch_out, 3)
         #     self.convs.append(nn.Conv2d(num_ch_enc[i], num_ch_enc[i+1], 1))
         # self.fpn = nn.ModuleList(self.convs)
 
@@ -28,9 +28,13 @@ class FPN(nn.Module):
         :return [list]: List of feature maps that is the same length as input_features where the number of channels of
         each feature map matches self.num_ch_pyramid.
         """
-        pyramid = []
-        for i in range(len(input_features) - 1):
-            pyramid.append(self.upsampler(input_features[i+1]) + self.convs[i](input_features[i]))
-        pyramid.append(input_features[-1])
-
+        pyramid = [None] * len(input_features)
+        # pyramid[-1] = self.layers[str(len(input_features) - 1) + "_1x1conv"](input_features[-1])
+        for i in range(len(input_features) - 1, -1, -1):
+            curr_layer = self.layers[str(i) + "_1x1conv"](input_features[i])
+            if i != len(input_features) - 1:
+                curr_layer += self.upsampler(pyramid[i+1])
+            pyramid[i] = self.layers[str(i) + "_3x3conv"](curr_layer)
+            # pyramid.append(self.upsampler(input_features[i+1]) + self.convs[i](input_features[i]))
+        # pyramid.append(input_features[-1])
         return pyramid
